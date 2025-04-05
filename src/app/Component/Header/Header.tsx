8a2697a5b07fc6af68cd49cc458483fb5345ea45
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-04-05 23:06:03 +0900

    feat/기본 메인화면 스타일링

---
+++ src/app/Component/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.scss'
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            헤더
+        </header>
+    )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Main from "./Component/Main/Main";
 import ScheduleTable from "./Component/ScheduleTable/ScheduleTable";
 
 export default function Home() {
   return (
     <>
+      <Main/>
       <ScheduleTable/>
     </>
   );
