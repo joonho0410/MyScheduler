5adb011d70eb8a708cfc62cd880d0983334c6403
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-04-07 17:52:27 +0900

    feat/create Todo Componnt Prototype, Types#5

---
+++ src/Store/TodoStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { TodoType } from "@/Types/Todo";
+
+interface TodoState {
+    todoList: TodoType[]
+    currentTodo: TodoType | null
+    addTodo: (todo: TodoType) => void;
+    deleteTodo: (todo: TodoType) => void;
+    updateTodo: (todo: TodoType) => void;
+    startTodo: (todo: TodoType) => void;
+    endTodo: (todo: TodoType) => void;
+}
+
+const useTodoStore = create<TodoState>()((set) => ({
+    todoList: [],
+    currentTodo: null,
+    addTodo: (todo) => set((state) => ({...state, todoList: [...state.todoList, todo]})),
+    deleteTodo: (todo) => set((state) => ({...state, todoList: state.todoList.filter((e) => e !== todo)})),
+    updateTodo: (todo) => set((state) => state),
+    startTodo: (todo) => set((state) => ({...state, currentTodo: todo})),
+    endTodo: (todo) => set((state) => ({...state, currentTodo: null}))
+}))
+
+export default useTodoStore
