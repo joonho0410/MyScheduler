1d74af526131d76ed77f320d7fcb10b0a10c5218
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-04-07 22:56:29 +0900

    feat/inser modal div at layout & Main #5

--- src/app/Component/Main/Main.tsx
+++ src/app/Component/Main/Main.tsx
@@ -1,9 +1,21 @@
+'use client'
+
+import useModalStore from '@/Store/ModalStore'
+import Modals from '../Modals/Modals'
 import styles from './Main.module.scss'
+import dynamic from 'next/dynamic'
+
+const ModalContainer = dynamic(() => import('../Modals/ModalContainer'), { ssr: false })
 
 const Main = () => {
+    const { modalList, setModal } = useModalStore()
+
     return (
         <div className={styles.main}>
-            MainTheme
+            <button onClick={() => setModal('create_Todo')}> 할일 추가 </button>
+            <ModalContainer>
+                <Modals/>
+            </ModalContainer>
         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div id='modal'>
+        </div>
         <Header/>
         {children}
       </body>
