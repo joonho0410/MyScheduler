3cf82a1b5c37253d1cf71a1cd49d2f45ed99216b
joonho0410 <76806109+joonho0410@users.noreply.github.com>
2025-04-07 17:03:23 +0900

    Feat/기본 레이아웃 설정#3 (#4)
    * chore/Style 파일 절대 경로 세팅
    
    * feat/기본 메인화면 스타일링
    
    * style/ fix timeTable style
    
    ---------
    
    Co-authored-by: Junho Jeon <junhjeon@c1r1s5.42seoul.kr>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/Style')],  // 'src/Style' 폴더를 절대 경로로 지정
+  },
 };
 
 export default nextConfig;
---
+++ src/app/Component/Main/Main.tsx
@@ -0,0 +1,11 @@
+import styles from './Main.module.scss'
+
+const Main = () => {
+    return (
+        <div className={styles.main}>
+            MainTheme
+        </div>
+    )
+}
+
+export default Main
