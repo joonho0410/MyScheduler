edec5794751e6dd48af512532ba18f60076e17d6
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-04-18 23:02:17 +0900

    feat/ BasicBox, BasicButton 생성 및 적용 #16

--- src/app/Component/Main/Main.tsx
+++ src/app/Component/Main/Main.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import { useModalActions } from '@/Store/ModalStore'
+import { Button } from '@mui/material'
 import Modals from '../Modals/Modals'
 import dynamic from 'next/dynamic'
 import styles from './Main.module.scss'
+import BasicButton from '../Common/BasicButton'
 
 const ModalContainer = dynamic(() => import('../Modals/ModalContainer'), { ssr: false })
 
@@ -12,8 +14,26 @@ const Main = () => {
 
     return (
         <div className={styles.main}>
-            <button onClick={() => setModal('controll_Todo')}> 할일 추가 </button>
-            <button onClick={() => setModal('show_Today')}> 한일 보기 </button>
+            <BasicButton 
+                onClick={() => setModal('controll_Todo')}
+                sx={{
+                    position: 'absolute',
+                    top: '40px',
+                    right: '40px'
+                }}
+            >
+                할 일 추가
+            </BasicButton>
+            <BasicButton 
+                onClick={() => setModal('show_Today')}
+                sx={{
+                    position: 'absolute',
+                    bottom: '40px',
+                    right: '40px'
+                }}
+            >
+                한 일 보기
+            </BasicButton>
             <ModalContainer>
                 <Modals/>
             </ModalContainer>
