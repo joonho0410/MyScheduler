50b1fadfd5285409183cecb3623e64e49519c857
Junho Jeon <junhjeon@c1r1s5.42seoul.kr>
2025-04-29 22:06:35 +0900

    fix/ 불필요한 로직 수정

--- src/Hooks/useDragMove.ts
+++ src/Hooks/useDragMove.ts
@@ -15,7 +15,7 @@ const useDragMove = (
   const [isDragging, setIsDragging] = useState(false);
 
   const handleMouseDown = (ev: React.MouseEvent<HTMLElement> | React.TouchEvent<HTMLElement>) => {
-    ev.preventDefault();
+    if (ev instanceof MouseEvent) ev.preventDefault();
     const startX = 'clientX' in ev ? ev.clientX : ev.touches[0].clientX;
     setStart({ x: startX });
     setIsDragging(true);
